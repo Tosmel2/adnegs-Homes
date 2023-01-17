392c9ecc44c3c43d9edb7c856e1a43da99b19bc5
tosmel2 <adewaletosin0808@gmail.com>
2023-01-17 09:22:56 +0100

    about component and header component fixed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,55 @@
 import React from 'react'
+import ml1 from '../assets/ml1.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faLeaf, faCarrot, faGem, faUser, faAppleAlt } from '@fortawesome/free-solid-svg-icons'
+
+
+const data = [
+  {iconUrl: 'icon1', text: 'Pay as Little as Posible!'},
+  {iconUrl: 'icon2', text: 'Enjoy wisdom of community!'},
+  {iconUrl: 'icon3', text: "Let's somebody else take care Landlord!"},
+  {iconUrl: 'icon4', text: 'Enjoy peaceful Environment!'},
+  {iconUrl: 'icon5', text: 'Stay Safe! Save Money!'},
+  {iconUrl: 'icon6', text: 'Pay for what you use !'}
+]
 
 const About = () => {
+
+  const icons = {
+    'icon1': faCoffee,
+    'icon2': faLeaf,
+    'icon3': faCarrot,
+    'icon4': faGem,
+    'icon5': faUser,
+    'icon6': faAppleAlt
+  }
+
   return (
     <div className='about_container'>
       <div className='row'>
-        About
+        <h1 className='pl-5 text-2xl font-bold' style={{border:'2px solid red'}}>minimum living cost takes care of everything</h1>
+
+        <div className='about-box'>
+
+          <div className="about-box-img">
+            <img src={ml1} className='testimony_img' alt='ml1'/>
+          </div>
+
+          <div className="about-box-divs flex items-center">
+            <div className="flex flex-wrap justify-between">
+              {data.map((item, index) => (
+                <div className="map_items-box py-8 px-3" key={index} style={{width:'49%', boxShadow: '0px 12px 19px rgba(0, 0, 0, 0.15)'}}>
+                  <FontAwesomeIcon icon={icons[item.iconUrl]} size="lg" className="text-center my-icon" color="red" />
+                  <p className="font-bold text-start">{item.text}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
 export default About
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,27 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <nav>
+        <div className="logo row">TESTING</div>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">LandLord</a></li>
+          <li><a href="/">Tenants</a></li>
+          <li><a href="/">Contact Us</a></li>
+        </ul>
+      </nav>
+      <div className="header-content row">
+        <h1 className='heading-text'>The Most Affortable Place To stay In The San Franciso Bay Area</h1>
+        <div className="heading-map">
+        <iframe className="position-relative rounded w-full h-100" 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.8851856965694!2d3.34320711426848!3d7.139273617680197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x61fc9ec0c3d0a852!2sNYSC%20Mini%20Secretariat%20%2C%20Oke%20Ilewo%2C%20Abeokuta%20Ogun%20State!5e0!3m2!1sen!2sng!4v1640728238812!5m2!1sen!2sng"
+          frameborder="0" style={{minheight: 350, border:0}} allowfullscreen="" aria-hidden="false"
+          tabindex="0" title="myMap">
+        </iframe>
+        </div>
+      </div>
+    </div>
   )
 }
 
